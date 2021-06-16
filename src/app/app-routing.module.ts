7b6828a885366bc38e10a684eef21d452e6823da
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-06-16 14:38:31 +0300

    authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
+import { EmailComponent } from './User/email/email.component';
+import { ProfileComponent } from './User/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'email-login', component: EmailComponent },
+  { path: 'signup', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
+import { EmailComponent } from './User/email/email.component';
+import { ProfileComponent } from './User/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    EmailComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
