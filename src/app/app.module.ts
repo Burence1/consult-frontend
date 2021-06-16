bd370756fad41e5f5e954b29bf95863470b3a797
Derrick-Nyongesa <78686755+Derrick-Nyongesa@users.noreply.github.com>
2021-06-16 15:17:04 +0300

    Derrick authentication (#1)
    * created authetication components
    
    * authentication
    
    * authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3mBUPgJ-drLGaMass8FdKRyJzToqEOa4",
+  authDomain: "userauth-3c76a.firebaseapp.com",
+  projectId: "userauth-3c76a",
+  storageBucket: "userauth-3c76a.appspot.com",
+  messagingSenderId: "329320770247",
+  appId: "1:329320770247:web:b0d6211ac58834b457ace2",
+  measurementId: "G-DXEDLCP9XL"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFirestore,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/authentication/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  // loginGoogle() {
+  //   this.authService.googleLogin();
+  // }
+
+}
