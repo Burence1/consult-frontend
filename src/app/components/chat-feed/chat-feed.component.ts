41aa88b876b0125bfeaf0e0fa8f694e6a0d3d958
Burence1 <burensomondi@gmail.com>
2021-06-17 19:26:20 +0300

    add chat-feed display message logic

--- src/app/components/chat-feed/chat-feed.component.ts
+++ src/app/components/chat-feed/chat-feed.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnChanges } from '@angular/core';
+import { ChatService } from 'src/app/services/chat/chat.service';
+import { Observable } from 'rxjs';
+import { AngularFireList } from '@angular/fire/database';
+import { Chatmessage } from 'src/app/classes/message/chatmessage';
+
 
 @Component({
   selector: 'app-chat-feed',
   templateUrl: './chat-feed.component.html',
   styleUrls: ['./chat-feed.component.css']
 })
-export class ChatFeedComponent implements OnInit {
+export class ChatFeedComponent implements OnInit,OnChanges {
 
-  constructor() { }
+  feed: Observable<any[]>;
+
+  constructor(private chat: ChatService) { }
 
   ngOnInit(): void {
+    this.feed = this.chat.getMessages().valueChanges();
   }
 
+  ngOnChanges(): void {
+    this.feed = this.chat.getMessages().valueChanges();
+  }
 }
