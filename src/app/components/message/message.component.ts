6eb3461e889ed28ebf00f07855ee37460b99f380
Burence1 <burensomondi@gmail.com>
2021-06-17 19:44:21 +0300

    add message class, add message crud methods

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ChatService } from 'src/app/services/chat/chat.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Chatmessage } from 'src/app/classes/message/chatmessage';
 
 @Component({
   selector: 'app-message',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  @Input() chatMessage: Chatmessage;
+  userEmail?: string;
+  userName?: string;
+  messageContent?: string;
+  timeStamp?: Date = new Date();
+  isOwnMessage: boolean;
+  ownEmail: string;
 
-  ngOnInit(): void {
+
+  constructor(private auth: AuthService) {
+    // auth.authUser().subscribe((user: { email: string; }) => {
+    //   this.ownEmail = user.email;
+    //   this.isOwnMessage = this.ownEmail === this.userEmail;
+    // });
   }
 
+  ngOnInit(chatMessage = this.chatMessage) {
+    this.messageContent = chatMessage.message;
+    this.timeStamp = chatMessage.timeSent;
+    this.userEmail = chatMessage.email;
+    this.userName = chatMessage.userName;
+  }
 }
