416041879ee8df7f60905d49979b69d700391c59
Kasparov <ianjkiplagat@gmail.com>
2021-06-19 21:17:39 +0300

    add form validators

--- src/app/components/authentication/register/register.component.ts
+++ src/app/components/authentication/register/register.component.ts
@@ -1,17 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth/auth.service';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import { ToastrService } from 'ngx-toastr';
 
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -21,11 +12,9 @@ export class RegisterComponent implements OnInit {
 
   hide = true;
 
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email: any;
   password: any;
   displayName: any;
-
-  matcher = new MyErrorStateMatcher();
 
   constructor(public authService: AuthService, private toastr: ToastrService) { }
 
