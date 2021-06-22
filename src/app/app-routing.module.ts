51ef4b5c403f2770751b1dbff5b9d667adc2b4b2
el <elrophilskwaila@gmail.com>
2021-06-22 12:38:36 +0300

    mat-list removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ForgotPasswordComponent } from './components/authentication/forgot-password/forgot-password.component';
 import { ChatroomComponent } from './components/chatroom/chatroom.component';
@@ -22,7 +21,6 @@ const routes: Routes = [
   { path: 'chatroom', component: ChatroomComponent },
   { path: 'password_reset', component: ForgotPasswordComponent },
   { path: 'tasks', component: TasksComponent },
-  {path: 'dashboard', component: DashboardComponent},
   { path: '**', component: NotFoundComponent },
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,7 +45,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
