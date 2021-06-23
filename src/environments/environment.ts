de7b773b34b437ebd6248f9f4577f773c0e54a94
Burence1 <burensomondi@gmail.com>
2021-06-23 12:11:57 +0300

    add angular material extensions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,25 +5,25 @@
 export const environment = {
   production: false,
 
-    firebase: {
-      apiKey: 'AIzaSyB3mBUPgJ-drLGaMass8FdKRyJzToqEOa4',
-      authDomain: 'userauth-3c76a.firebaseapp.com',
-      projectId: 'userauth-3c76a',
-      storageBucket: 'userauth-3c76a.appspot.com',
-      messagingSenderId: '329320770247',
-      appId: '1:329320770247:web:b0d6211ac58834b457ace2',
-      measurementId: 'G-DXEDLCP9XL'
-    }
+    // firebase: {
+    //   apiKey: 'AIzaSyB3mBUPgJ-drLGaMass8FdKRyJzToqEOa4',
+    //   authDomain: 'userauth-3c76a.firebaseapp.com',
+    //   projectId: 'userauth-3c76a',
+    //   storageBucket: 'userauth-3c76a.appspot.com',
+    //   messagingSenderId: '329320770247',
+    //   appId: '1:329320770247:web:b0d6211ac58834b457ace2',
+    //   measurementId: 'G-DXEDLCP9XL'
+    // }
 
-  // firebase: {
-  //   apiKey: "AIzaSyAmf-iuESeQTocOFJRd-UJKXro3S1qhu0c",
-  //   authDomain: "fire-chat-d5ddf.firebaseapp.com",
-  //   projectId: "fire-chat-d5ddf",
-  //   storageBucket: "fire-chat-d5ddf.appspot.com",
-  //   messagingSenderId: "951859129045",
-  //   appId: "1:951859129045:web:3f82d3ebc152a72eb5505c",
-  //   measurementId: "G-5Z2M1P8W2B"
-  // }
+  firebase: {
+    apiKey: "AIzaSyAmf-iuESeQTocOFJRd-UJKXro3S1qhu0c",
+    authDomain: "fire-chat-d5ddf.firebaseapp.com",
+    projectId: "fire-chat-d5ddf",
+    storageBucket: "fire-chat-d5ddf.appspot.com",
+    messagingSenderId: "951859129045",
+    appId: "1:951859129045:web:3f82d3ebc152a72eb5505c",
+    measurementId: "G-5Z2M1P8W2B"
+  }
 };
 
 /*
