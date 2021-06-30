52fecdf778661ca1f039a346bbe0e1c93d343822
Burence1 <burensomondi@gmail.com>
2021-07-01 01:27:51 +0300

    add required angular material extensions

--- src/app/components/addusers/addusers.component.ts
+++ src/app/components/addusers/addusers.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+// import * as firebase from 'firebase';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AdditionalChatServiceService } from 'src/app/services/additional/additional-chat-service.service';
 
 @Component({
   selector: 'app-addusers',
