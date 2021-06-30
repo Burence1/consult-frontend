82a95d393c83c2d3e32a1ec9dae798ab55f4be33
Burence1 <burensomondi@gmail.com>
2021-07-01 01:42:16 +0300

    add dynamic displayname and end to end coversation encryption

--- src/app/components/convolist/convolist.component.ts
+++ src/app/components/convolist/convolist.component.ts
@@ -25,9 +25,118 @@ export class ConvolistComponent implements OnInit {
   receiver: any
   userid: string
 
-  constructor() { }
+  constructor(private Auth: AngularFireAuth, private db: AngularFireDatabase, private route: ActivatedRoute, private router: Router, private auth: AuthService, private chat: ChatService) {
+    this.Auth.authState.subscribe(auth => {
+      if (auth !== undefined && auth !== null) {
+        this.user = auth;
+        this.userid = this.user.uid
+
+        firebase.database().ref('conversations/').on('value', resp => {
+          //this.conversations = [];
+          console.log(this.user.uid)
+          this.conversations = this.snapshotToArray(resp, this.userid);
+          console.log(this.conversations)
+          this.isLoadingResults = false;
+        });
+
+      }
+      this.getUser().valueChanges().subscribe(a => {
+        this.userName = a;
+        this.chatname = this.userName.displayName
+      });
+    });
+
+    // firebase.database().ref('conversations/').on('value', (snapshot: any) => {
+    //   snapshot.forEach((childSnapshot: any) => {
+    //     var childKey = childSnapshot.key;
+    //     var childData = childSnapshot.val();
+    //     this.conversations=[]
+    //     this.conversations = childData
+    //     console.log(this.conversations)
+    //     this.userId = this.conversations.sender
+    //     console.log(this.userId)
+    //     this.receiver = this.conversations.receiver
+    //     console.log(this.receiver)
+
+    //   });
+    // });
+
+  }
+
+  getUser() {
+    const userId = this.user.uid;
+    const path = `/users/${userId}`;
+    return this.db.object(path);
+  }
+
+  getCurrent(userId: string) {
+    let currentuser
+    firebase.database().ref('users/').on('value', (resp: any) => {
+      resp.forEach((child: any) => {
+        console.log(child.val())
+        console.log(child.key)
+        if (child.key === userId) {
+          return child.val().displayName
+        }
+      })
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  snapshotToArray = (snapshot: any, userId: string) => {
+
+    const returnArr: any[] = [];
+
+    snapshot.forEach((childSnapshot: any) => {
+      let item = childSnapshot.val();
+      item.key = childSnapshot.key;
+      const show = userId === item.sender || userId === item.receiver
+
+      if (userId === item.sender) {
+        firebase.database().ref('users/').on('value', (resp: any) => {
+          resp.forEach((child: any) => {
+            console.log(child.val())
+            console.log(child.key)
+            if (child.key === item.receiver) {
+              item = { ...item, show, displayName: child.val().displayName }
+              if (show) {
+                returnArr.push(item)
+              }
+            }
+          })
+        });
+      }
+      else {
+        firebase.database().ref('users/').on('value', (resp: any) => {
+          resp.forEach((child: any) => {
+            console.log(child.val())
+            console.log(child.key)
+            if (child.key === item.sender) {
+              item = { ...item, show, displayName: child.val().displayName }
+              if (show) {
+                returnArr.push(item)
+              }
+            }
+          })
+        });
+      }
+      //const name = (userId === item.sender)?this.getCurrent(item.receiver):this.getCurrent(item.sender)
+
+    });
+
+    return returnArr;
+  };
+
+  enterChatRoom(displayName: string) {
+
+    firebase.database().ref('recipients/').orderByChild('displayName').equalTo(displayName).on('value', (resp: any) => {
+      let recipient = [];
+      recipient = this.snapshotToArray(resp, this.userid);
+      const user = recipient.find(x => x.chatname === this.chatname);
+    });
+    this.router.navigate(['/chatroom', displayName]);
   }
 
 }
