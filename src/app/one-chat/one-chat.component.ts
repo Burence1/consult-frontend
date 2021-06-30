868feff7438604096c33c0c7a1fb18863b4bca4b
Burence1 <burensomondi@gmail.com>
2021-07-01 01:01:36 +0300

    add get conversations method

--- src/app/one-chat/one-chat.component.ts
+++ src/app/one-chat/one-chat.component.ts
@@ -34,14 +34,110 @@ export const snapshotToArray = (snapshot: any) => {
 })
 export class OneChatComponent implements OnInit {
  
+  @ViewChild('chatcontent') chatcontent: ElementRef;
+  scrolltop: any | null;
 
-  constructor() { 
-    
+  updateM: Chatmessage = {
+    message: ''
+  }
+
+  chatForm: FormGroup;
+  chatname = '';
+  roomname = '';
+  message = '';
+  users: any[];
+  chats: any[];
+  user: any;
+  userName: any;
+  messages: any
+  matcher = new MyErrorStateMatcher();
+  conversations: any;
+  convoname: any
+
+  constructor(private Auth: AngularFireAuth, private db: AngularFireDatabase, private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder) {
+    this.Auth.authState.subscribe(auth => {
+      if (auth !== undefined && auth !== null) {
+        this.user = auth;
+      }
+      this.getUser().valueChanges().subscribe(a => {
+        this.userName = a;
+        this.chatname = this.userName.displayName
+      });
+
+      firebase.database().ref('conversations/').on('value', (snapshot: any) => {
+        snapshot.forEach((childSnapshot: any) => {
+          let childKey = childSnapshot.key;
+          let childData = childSnapshot.val();
+          this.convoname = childData.convoname
+          console.log(this.convoname)
+
+        })
+      });
+
+      this.roomname = this.route.snapshot.params.roomname;
+      firebase.database().ref('messages/').on('value', resp => {
+        let chats = snapshotToArray(resp);
+        this.chats = chats.filter(x => x.name === this.convoname)
+        setTimeout(() => this.scrolltop = this.chatcontent.nativeElement.scrollHeight, 500);
+      });
+      firebase.database().ref('roomusers/').orderByChild('roomname').equalTo(this.roomname).on('value', (resp2: any) => {
+        const roomusers = snapshotToArray(resp2);
+        this.users = roomusers.filter(x => x.status === 'online');
+      });
+    })
+  }
+
+  getUser() {
+    const userId = this.user.uid;
+    const path = `/users/${userId}`;
+    return this.db.object(path);
   }
 
 
   ngOnInit(): void {
-    
+    this.chatForm = this.formBuilder.group({
+      'message': [null, Validators.required]
+    });
+
+  }
+  name: any
+  onFormSubmit(form: any) {
+    var day = new Date();
+    const options = { day: 'numeric', month: 'long', year: "numeric", timeZone: "Asia/Kolkata" } as const;
+    const today = day.toLocaleDateString("en-IN", options);
+
+    const chat = form;
+    chat.roomname = this.roomname;
+    console.log(chat.roomname)
+    chat.chatname = this.chatname;
+    chat.sender = this.user.displayName
+    chat.name = this.convoname
+    console.log(this.name)
+    chat.date = today;
+    chat.type = 'message';
+    const newMessage = firebase.database().ref('messages/').push();
+    newMessage.set(chat);
+    this.chatForm = this.formBuilder.group({
+      'message': [null, Validators.required]
+    });
+  }
+
+  deleteMsg(uid: any) {
+    const key = uid
+    var del = confirm("Want to delete?");
+    if (del) {
+      firebase.database().ref(`messages/${key}`).remove();
+    }
+  }
+
+  UpdateMsg(uid: any) {
+    const key = uid
+    var del = confirm("Want to delete?");
+    if (del) {
+      firebase.database().ref(`chats/${key}`).update(this.updateM);
+    }
   }
  
 }
