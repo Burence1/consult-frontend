7992325e3c7ef80f0ac6f3d7e642204e984bdf52
Burence1 <burensomondi@gmail.com>
2021-07-01 01:00:21 +0300

    add one-chat imports and child snapshot method

--- src/app/one-chat/one-chat.component.ts
+++ src/app/one-chat/one-chat.component.ts
@@ -1,4 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Chatmessage } from '../classes/message/chatmessage';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import firebase from 'firebase/app';
+import 'firebase/auth'
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export const snapshotToArray = (snapshot: any) => {
+  const returnArr: any[] = [];
+
+  snapshot.forEach((childSnapshot: any) => {
+    const item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+
+  return returnArr;
+};
 
 @Component({
   selector: 'app-one-chat',
@@ -16,6 +43,5 @@ export class OneChatComponent implements OnInit {
   ngOnInit(): void {
     
   }
-
  
 }
