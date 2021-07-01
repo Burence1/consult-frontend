67828268814ba04dbcd8e8c0de8ac5e5eb810675
Kasparov <ianjkiplagat@gmail.com>
2021-07-01 08:57:01 +0300

    merge with dev

--- src/app/profile.ts
+++ src/app/profile.ts
@@ -1,5 +1,4 @@
 export class Profile {
-<<<<<<< HEAD
   id: number;
   displayName: string;
   email: string;
@@ -13,17 +12,4 @@ export class Profile {
   interest3?: string;
   interest4?: string;
   bio?: string;
-=======
-    id: number;
-    displayName: string;
-    email: string;
-    status: string;
-    image?: any;
-    department?: string;
-    position?: string;
-    contact?: number;
-    interest1?: string;
-    bio?: string;
-
->>>>>>> cabdbde1cc003b86a4aab18723d329793d3c6649
 }
